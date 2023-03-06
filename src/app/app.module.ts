a7e7dbc62c2694b355144fb9f60092cb5373f54d
vtev <opqwerzxc@gmail.com>
2023-03-06 20:35:41 +0100

    added website structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AboutPageModule } from './about-page/about-page.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactPageModule } from './contact-page/contact-page.module';
+import { DirectionsPageModule } from './directions-page/directions-page.module';
+import { FooterModule } from './footer/footer.module';
+import { HeaderModule } from './header/header.module';
+import { MainPageModule } from './main-page/main-page.module';
+import { PartnersPageModule } from './partners-page/partners-page.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HeaderModule,
+    FooterModule,
+    MainPageModule,
+    AboutPageModule,
+    PartnersPageModule,
+    DirectionsPageModule,
+    ContactPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
