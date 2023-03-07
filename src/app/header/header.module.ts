357186ea6420d28524eef2fc1caec9743bd04ef5
vtev <opqwerzxc@gmail.com>
2023-03-07 19:59:18 +0100

    flattened header component

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header.component';
 import { LogoComponent } from './logo/logo.component';
 import { ButtonComponent } from './button/button.component';
 
