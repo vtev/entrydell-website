2df50001d80cb8a10cbabfeb3a81c95f9722609b
vtev <opqwerzxc@gmail.com>
2023-03-12 17:24:06 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { DirectionsPageComponent } from './directions-page/directions-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { PartnersPageComponent } from './partners-page/partners-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'directions', component: DirectionsPageComponent },
+  { path: 'partners', component: PartnersPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', redirectTo: '/main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
