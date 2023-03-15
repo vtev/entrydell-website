51fc17dd605b2a045c69ae698e85af1ee7b14715
Artemiy4884 <91424291+Artemiy4884@users.noreply.github.com>
2023-03-15 22:22:16 +0300

    added shared button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UniversalButtonComponent } from './universal-button/universal-button.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UniversalButtonComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    UniversalButtonComponent
   ]
 })
 export class SharedModule { }
