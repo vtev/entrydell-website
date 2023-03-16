e28588308b0784677284a1dda614e34864009c2a
vtev <opqwerzxc@gmail.com>
2023-03-16 21:00:22 +0100

    added directions on main page

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
 import { TopBannerComponent } from './top-banner/top-banner.component';
+import { DirectionComponent } from './direction/direction.component';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    TopBannerComponent
+    TopBannerComponent,
+    DirectionComponent
   ],
   imports: [
     CommonModule
