ff67483225ade04c7521f85eb8e8dd8259d3f883
Artemiy4884 <91424291+Artemiy4884@users.noreply.github.com>
2023-04-10 01:41:35 +0300

    added about page
    ---------
    
    Co-authored-by: vtev <vtev@tutanota.com>

--- src/app/about-page/about-page.module.ts
+++ src/app/about-page/about-page.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './about-page.component';
 import {ButtonComponent} from "./button/button.component";
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { OfficesAndCompaniesComponent } from './offices-and-companies/offices-and-companies';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    ButtonComponent
+    ButtonComponent,
+    OfficesAndCompaniesComponent,
+    AboutCompanyComponent
   ],
   imports: [
     CommonModule
