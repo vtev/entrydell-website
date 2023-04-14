58f0fd9a5283c6c214a4171b632f83565e2f4392
Artemiy4884 <91424291+Artemiy4884@users.noreply.github.com>
2023-04-14 15:18:06 +0300

    added 3 direction pages

---
+++ src/app/engineering-page/engineering-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-engineering-page',
+  templateUrl: './engineering-page.component.html',
+  styleUrls: ['./engineering-page.component.css']
+})
+export class EngineeringPageComponent {
+
+}
---
+++ src/app/pharmacology-page/pharmacology-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pharmacology-page',
+  templateUrl: './pharmacology-page.component.html',
+  styleUrls: ['./pharmacology-page.component.css']
+})
+export class PharmacologyPageComponent {
+
+}
