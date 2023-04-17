a79eb0569dc6a3b0968883178f423b313f4d8b67
vtev <vtev@tutanota.com>
2023-04-17 18:30:11 +0200

    removed obsolete universal button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UniversalButtonComponent } from './universal-button/universal-button.component';
 import { PartnerComponent } from './partner/partner.component';
 
 
 
 @NgModule({
   declarations: [
-    UniversalButtonComponent,
     PartnerComponent
   ],
   imports: [
     CommonModule
   ],
-    exports: [
-        UniversalButtonComponent,
-        PartnerComponent
-    ]
+  exports: [
+    PartnerComponent
+  ]
 })
 export class SharedModule { }
