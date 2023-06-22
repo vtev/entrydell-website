1f77a520301652ff707787b12b8544a8a85ae89f
Artemiy4884 <artemiytev@gmail.com>
2023-06-22 22:36:21 +0300

    Added full screen mode for the pictures

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import {Fancybox} from "@fancyapps/ui";
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+Fancybox.bind("[data-fancybox]", { });
