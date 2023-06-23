9ff3cabf909016f094bbbe782a0137ed93b1790a
Artemiy4884 <artemiytev@gmail.com>
2023-06-23 17:54:08 +0300

    Fixed image animation opacity

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,10 @@ import {Fancybox} from "@fancyapps/ui";
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-Fancybox.bind("[data-fancybox]", { });
+Fancybox.bind("[data-fancybox]", {
+    // Images : {
+    //   zoomOpacity : 'auto',
+    // }
+    // animated : false,
+});
+
